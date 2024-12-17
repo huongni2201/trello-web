bd8f6aeb935ad04a8c0405a2afa89d116ea1ec23
Huong Nguyen <huongnn2201@gmail.com>
2024-12-17 21:43:17 +0700

    Add import eslint

--- eslint.config.js
+++ eslint.config.js
@@ -24,6 +24,7 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      // React
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
@@ -33,6 +34,34 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+
+      // MUI
+      'no-restricted-imports': [
+        'error',
+        {
+          'patterns': ['@mui/*/*/*']
+        }
+      ],
+
+      // Common
+      'no-console': 1,
+      'no-lonely-if': 1,
+      'no-unused-vars': 1,
+      'no-trailing-spaces': 1,
+      'no-multi-spaces': 1,
+      'no-multiple-empty-lines': 1,
+      'space-before-blocks': ['error', 'always'],
+      'object-curly-spacing': [1, 'always'],
+      'indent': ['warn', 2],
+      'semi': [1, 'never'],
+      'quotes': ['error', 'single'],
+      'array-bracket-spacing': 1,
+      'linebreak-style': 0,
+      'no-unexpected-multiline': 'warn',
+      'keyword-spacing': 1,
+      'comma-dangle': 1,
+      'comma-spacing': 1,
+      'arrow-spacing': 1
     },
   },
 ]
